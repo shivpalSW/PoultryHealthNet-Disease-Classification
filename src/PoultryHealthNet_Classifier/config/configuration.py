e4e6f184e656b68e3af9ac9a970c918e4a59c184
shivpalSW <shivpal.wadkar2050@gmail.com>
2023-08-08 14:17:56 +0530

    prepare callbacks added

--- src/PoultryHealthNet_Classifier/config/configuration.py
+++ src/PoultryHealthNet_Classifier/config/configuration.py
@@ -1,7 +1,9 @@
 from PoultryHealthNet_Classifier.constants import *
+import os 
 from PoultryHealthNet_Classifier.utils.common import read_yaml,create_directories
 from PoultryHealthNet_Classifier.entity.config_entity import (DataIngestionConfig,
-                                                              PrepareBaseModelConfig)
+                                                              PrepareBaseModelConfig,
+                                                              PrepareCallbacksConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -48,3 +50,21 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
+
