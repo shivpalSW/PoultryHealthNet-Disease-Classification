53208b1f16762ada01822e6941711fe6c39a75db
shivpalSW <shivpal.wadkar2050@gmail.com>
2023-08-08 15:26:48 +0530

    stage_03_training added

--- src/PoultryHealthNet_Classifier/entity/config_entity.py
+++ src/PoultryHealthNet_Classifier/entity/config_entity.py
@@ -9,6 +9,7 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
     root_dir: Path
@@ -29,5 +30,14 @@ class PrepareCallbacksConfig:
     checkpoint_model_filepath: Path
 
 
-    
 
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
